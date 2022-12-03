160e213647946cb5c1b17444b923f60ac6632d05
xylous <xylous.e@gmail.com>
2022-12-03 23:15:26 +0200

    eval: add support for loops

--- src/Eval.hs
+++ src/Eval.hs
@@ -5,6 +5,7 @@ module Eval
 import Parser
 
 import Data.List (find)
+import Data.Maybe (fromJust)
 
 type Program = [Instruction]
 type Tape = [(Int, Int)]      -- list of cell indexes and their values
@@ -29,12 +30,36 @@ cellValue (Machine tape ptr) = do
     (_,v) <- find (\(k,_) -> k == ptr) tape
     Just v
 
-eval :: Machine -> Instruction -> Machine
+eval :: Machine -> Instruction -> Maybe Machine
 eval m i
-    | i == MoveR = Machine tape (ptr+1)
-    | i == MoveL = Machine tape (ptr-1)
-    | i == Increment = changeState (+1) m
-    | i == Decrement = changeState (subtract 1) m
+    | i == MoveR = Just $ Machine tape (ptr+1)
+    | i == MoveL = Just $ Machine tape (ptr-1)
+    | i == Increment = Just $ changeState (+1) m
+    | i == Decrement = Just $ changeState (subtract 1) m
+    | (not . null) iLoop = loop m iLoop
     | otherwise = undefined -- should never happen
       where
         (Machine tape ptr) = m
+        iLoop = loopContents i
+        current = fromJust $ cellValue m
+
+-- If the given instruction is a loop, return all instructions within it
+loopContents :: Instruction -> [Instruction]
+loopContents (Loop xs) = xs
+loopContents _ = []
+
+-- Execute loop only if the current cell's value is greater than zero and, pray
+-- to God your code is correct, maybe return the final state of the machine
+loop :: Machine -> [Instruction] -> Maybe Machine
+loop m is = do
+    m' <- execAll m is
+    current <- cellValue m
+    if current /= 0 then loop m' is else Just m
+
+-- Execute all instructions in the instructions chain and return the final state
+-- of the machine... Maybe. Just maybe.
+execAll :: Machine -> [Instruction] -> Maybe Machine
+execAll m (i:is) = do
+    m' <- eval m i
+    execAll m' is
+execAll m _ = Just m
