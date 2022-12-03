f8cfa0ca4c108efda75ec50200c8f4784e4af1bd
xylous <xylous.e@gmail.com>
2022-12-03 23:37:50 +0200

    Implement I/O operations, no error checking

--- src/Eval.hs
+++ src/Eval.hs
@@ -6,6 +6,7 @@ import Parser
 
 import Data.List (find)
 import Data.Maybe (fromJust)
+import Data.Char (chr)
 
 type Program = [Instruction]
 type Tape = [(Int, Int)]      -- list of cell indexes and their values
@@ -30,13 +31,22 @@ cellValue (Machine tape ptr) = do
     (_,v) <- find (\(k,_) -> k == ptr) tape
     Just v
 
-eval :: Machine -> Instruction -> Maybe Machine
+-- TODO: types look too ugly. Maybe try fixing them later?
+eval :: Machine -> Instruction -> IO (Maybe Machine)
 eval m i
-    | i == MoveR = Just $ Machine tape (ptr+1)
-    | i == MoveL = Just $ Machine tape (ptr-1)
-    | i == Increment = Just $ changeState (+1) m
-    | i == Decrement = Just $ changeState (subtract 1) m
+    | i == MoveR = return . Just $ Machine tape (ptr+1)
+    | i == MoveL = return . Just $ Machine tape (ptr-1)
+    | i == Increment = return . Just $ changeState (+1) m
+    | i == Decrement = return . Just $ changeState (subtract 1) m
     | (not . null) iLoop = loop m iLoop
+    | i == Output = do
+        print $ chr current
+        return (Just m)
+    | i == Input = do
+        line <- getLine
+        let newval = read line :: Int
+        let m' = changeState (+ (newval - current)) m
+        return . Just $ m'
     | otherwise = undefined -- should never happen
       where
         (Machine tape ptr) = m
@@ -50,16 +60,16 @@ loopContents _ = []
 
 -- Execute loop only if the current cell's value is greater than zero and, pray
 -- to God your code is correct, maybe return the final state of the machine
-loop :: Machine -> [Instruction] -> Maybe Machine
+loop :: Machine -> [Instruction] -> IO (Maybe Machine)
 loop m is = do
     m' <- execAll m is
-    current <- cellValue m
-    if current /= 0 then loop m' is else Just m
+    let current = fromJust $ cellValue m
+    if current /= 0 then loop (fromJust m') is else return . Just $ m
 
 -- Execute all instructions in the instructions chain and return the final state
 -- of the machine... Maybe. Just maybe.
-execAll :: Machine -> [Instruction] -> Maybe Machine
+execAll :: Machine -> [Instruction] -> IO (Maybe Machine)
 execAll m (i:is) = do
     m' <- eval m i
-    execAll m' is
-execAll m _ = Just m
+    execAll (fromJust m') is
+execAll m _ = return . Just $ m
