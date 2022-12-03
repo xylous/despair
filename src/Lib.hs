9cd8a50e88e6504f6ad56b890a0f3c75420883a7
xylous <xylous.e@gmail.com>
2022-12-03 19:10:01 +0200

    Add a function to parse instructions

--- src/Lib.hs
+++ src/Lib.hs
@@ -13,3 +13,16 @@ data Instruction = MoveR
 type Program = [Instruction]
 type Memory = [(Int, Int)]      -- list of cell indexes and their values
 type Pointer = Int
+
+toInstruction :: Char -> Instruction
+toInstruction ch
+    | ch == '>' = MoveR
+    | ch == '<' = MoveL
+    | ch == '+' = Increment
+    | ch == '-' = Decrement
+    | ch == '.' = Output
+    | ch == ',' = Input
+    | ch == '[' = JumpFw
+    | ch == ']' = JumpBw
+    -- Don't worry, this should never happen
+    | otherwise = undefined
