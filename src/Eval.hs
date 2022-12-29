1e981e04aabd25a3dad3b86c2f02e848bf768ad8
xylous <xylous.e@gmail.com>
2022-12-29 16:03:23 +0200

    Evaluator: use parser module for parsing files

--- src/Eval.hs
+++ src/Eval.hs
@@ -92,17 +92,9 @@ execAll m (i:is) = do
         Nothing -> return Nothing
 execAll m _ = return . Just $ m
 
--- Naively, keep only the character set of the language
-keepBFChars :: String -> String
-keepBFChars = filter (`elem` bfChars)
-
-bfChars :: [Char]
-bfChars = "+-><[].,"
-
 -- Evaluate the program in the given file and return the resulting machine
 evalFile :: FilePath -> IO (Maybe Machine)
-evalFile fileName = do
-    input <- readFile fileName
+evalFile path = do
+    is <- parseFile path
     let m = Machine (allocate 0 1) 0
-    let is = fromMaybe [] $ parse . keepBFChars $ input
     execAll m is
